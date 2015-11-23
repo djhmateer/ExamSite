da2a4f555b743a7453b7587e17d21e521280a87d
Dave Mateer <davemateer@gmail.com>
2015-11-23 06:19:09 +0000

    First commit

---
+++ ExamSite/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace ExamSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Async partial view
+        public async Task<ActionResult> List()
+        {
+            ViewBag.SyncOrAsync = "Asynchronous";
+            string results = string.Empty;
+            using (HttpClient httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync(new Uri("http://externalfeedsite"));
+                Byte[] downloadedBytes = await response.Content.ReadAsByteArrayAsync();
+                Encoding encoding = new ASCIIEncoding();
+                results = encoding.GetString(downloadedBytes);
+            }
+            return PartialView("partialViewName", results);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ ExamSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExamSite.Startup))]
+namespace ExamSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
